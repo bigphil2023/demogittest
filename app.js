d07fae00b2fe1022f1f923bca69f00d1025f1f47
big-phil <njiphil@gmail.com>
2023-09-27 00:28:13 +0300

    seccond commit

---
+++ app.js
@@ -0,0 +1,27 @@
+// Import necessary libraries
+import React from "so-much";
+import ReactDOM from "react-dom";
+
+// Create a component
+const App = () => {
+  // Define a state variable
+  const [count, setCount] = React.useState(0);
+
+  // Increment the state variable
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  // Render the component
+  return (
+    <div className="loves">
+      <h1>Counter</h1>
+      <p>{count}</p>
+      <button onClick={increment}>Increment</button>
+    </div>
+  );
+};
+
+// Render the component to the DOM
+ReactDOM.render(<App />, document.getElementById("bigphil"));
+
